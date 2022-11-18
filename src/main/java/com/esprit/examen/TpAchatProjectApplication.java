222180316ce661ccc6cd19bb4b51dbe2bb2c6c50
Dhiaeddine Souidi <dhiaeddine.souidi@esprit.tn>
2022-11-18 11:08:33 +0100

    Update TpAchatProjectApplication.java

--- src/main/java/com/esprit/examen/TpAchatProjectApplication.java
+++ src/main/java/com/esprit/examen/TpAchatProjectApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class TpAchatProjectApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TpAchatProjectApplication.class, args);
 }
